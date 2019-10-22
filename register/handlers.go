f899e76daa053692f1bd6cdb34e128ab85ed6908
XiovV <xiovv@tutanota.com>
2019-10-22 19:36:10 +0200

    checking if both passwords are the same

--- register/handlers.go
+++ register/handlers.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"text/template"
 )
 
 type SuccessfulResponse struct {
@@ -20,50 +19,79 @@ type FailedResponse struct {
 func HandleRegister(w http.ResponseWriter, r *http.Request) {
 	email := r.FormValue("email")
 	passwd := r.FormValue("password")
+	passwd_c := r.FormValue("password_confirm")
 
 	isEmailValid, err := IsEmailValid(email)
 	if err != nil {
 		response := FailedResponse{Error: err.Error()}
-		json.NewEncoder(w).Encode(response)
+		ReturnError(response, w)
 	}
 
 	isPasswordValid, err := IsPasswordValid(passwd)
 	if err != nil {
 		response := FailedResponse{Error: err.Error()}
-		json.NewEncoder(w).Encode(response)
+		ReturnError(response, w)
 	}
 
-	if isEmailValid == true && isPasswordValid == true {
-		b := DoesUserExist(email)
+	arePasswordsTheSame, err := ArePasswordsTheSame(passwd, passwd_c)
+	if err != nil {
+		response := FailedResponse{Error: err.Error()}
+		ReturnError(response, w)
+	}
 
-		if b == true {
-			response := FailedResponse{Error: "This user already exists"}
+	if arePasswordsTheSame == true {
+		if isEmailValid == true && isPasswordValid == true {
+			b := DoesUserExist(email)
 
-			json.NewEncoder(w).Encode(response)
-		} else {
-			hashedPasswd, err := HashPassword(passwd)
-			if err != nil {
-				log.Fatal(err)
+			if b == true {
+				response := FailedResponse{Error: "This user already exists"}
+
+				ReturnError(response, w)
+			} else {
+				hashedPasswd, err := HashPassword(passwd)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				newUser := User{email, hashedPasswd}
+
+				InsertUser(newUser)
+
+				jwt := GenerateToken(email)
+
+				response := SuccessfulResponse{Email: email, Token: jwt}
+
+				json.NewEncoder(w).Encode(response)
 			}
-
-			newUser := User{email, hashedPasswd}
-
-			InsertUser(newUser)
-
-			jwt := GenerateToken(email)
-
-			response := SuccessfulResponse{Email: email, Token: jwt}
-
-			json.NewEncoder(w).Encode(response)
 		}
-	} else {
-		fmt.Println(isEmailValid, isPasswordValid)
-		response := FailedResponse{Error: "Email or password are not valid"}
-		json.NewEncoder(w).Encode(response)
 	}
 }
 
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("register.html")
+	t, err := template.ParseFiles("static/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	t.Execute(w, nil)
+}
+
+func ServeCSS(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/css")
+	http.ServeFile(w, r, "static"+r.URL.String())
+}
+
+func ServeVendor(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/javascript")
+	http.ServeFile(w, r, "static"+r.URL.String())
+}
+
+func ServeJS(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/javascript")
+	http.ServeFile(w, r, "static"+r.URL.String())
+}
+
+func ServeFonts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/css")
+	http.ServeFile(w, r, "static"+r.URL.String())
 }

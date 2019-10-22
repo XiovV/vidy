413bcc1657a88c4bf38957e0ff6a74236cc655a8
XiovV <xiovv@tutanota.com>
2019-10-22 19:38:58 +0200

    added handlers for serving static files

--- login/handlers.go
+++ login/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"text/template"
 )
 
 type SuccessfulResponse struct {
@@ -16,6 +15,7 @@ type FailedResponse struct {
 }
 
 func HandleLogin(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	email := r.FormValue("email")
 	passwd := r.FormValue("password")
 
@@ -45,6 +45,26 @@ func HandleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("login.html")
-	t.Execute(w, nil)
+	w.Header().Set("Content-Type", "text/html")
+	http.ServeFile(w, r, "static/index.html")
+}
+
+func ServeCSS(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/css")
+	http.ServeFile(w, r, "static"+r.URL.String())
+}
+
+func ServeVendor(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/javascript")
+	http.ServeFile(w, r, "static"+r.URL.String())
+}
+
+func ServeJS(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/javascript")
+	http.ServeFile(w, r, "static"+r.URL.String())
+}
+
+func ServeFonts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/css")
+	http.ServeFile(w, r, "static"+r.URL.String())
 }
